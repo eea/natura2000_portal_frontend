7df5711a59176175af766fb029b618d41c3c80cf
Amanda Rodriguez <arodriguez@bilbomatica.es>
2024-11-28 11:58:57 +0100

    Fix download form validation

--- src/pages/Downloads.js
+++ src/pages/Downloads.js
@@ -268,7 +268,7 @@ const Downloads = () => {
                 const validateEmail = (email) => {
                     return email.toLowerCase().match(/^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/);
                 };
-                productErrors[field] = value.trim() === "" || validateEmail(value) ? false : true;
+                productErrors[field] = value && (value.trim() === "" || validateEmail(value)) ? false : true;
             }
             else {
                 productErrors[field] = !value ? true : false;
